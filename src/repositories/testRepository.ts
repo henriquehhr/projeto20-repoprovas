788ae93fcb723ebdd8c0f9be49dded1b4d964d01
Henrique Herbs Rosa <henriquehhr@gmail.com>
2022-07-24 14:50:53 -0300

    feat: added new test schema

--- src/repositories/testRepository.ts
+++ src/repositories/testRepository.ts
@@ -2,7 +2,7 @@ import prisma from "../config/database.js";
 import { Test } from "@prisma/client";
 
 export type CreateTest = Omit<Test, "id">;
-//TODO fazer validação de esquema de criação de teste
+
 export async function create(test: CreateTest) {
   await prisma.test.create({data: test});
 }
--- src/routes/testRouter.ts
+++ src/routes/testRouter.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { findAllGroupByOptions, create } from "../controllers/testController.js";
 import { authenticateUser } from "../middlewares/authenticateUser.js";
+import schemaValidator from "../middlewares/schemaValidator.js";
+import { newTestSchema } from "../schemas/newTestSchema.js";
 
 const testRouter = Router();
 
-testRouter.post("/tests", authenticateUser, create);
+testRouter.post("/tests", authenticateUser, schemaValidator(newTestSchema), create);
 testRouter.get("/tests", authenticateUser, findAllGroupByOptions);
 
 export default testRouter;
